96ab637d262bffc5cfda3cd9aeaf8ea237d13ca2
lin <kingxl777@gmail.com>
2026-01-01 23:21:45 +0800

    fix the headings with the icon

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,5 +1,6 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import { ImageZoom } from 'fumadocs-ui/components/image-zoom';
+import { Callout as BaseCallout, type CalloutProps } from 'fumadocs-ui/components/callout';
 import type { MDXComponents } from 'mdx/types';
 import { Mermaid } from '@/components/mdx/mermaid';
 
@@ -7,6 +8,13 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
     Mermaid,
+    Callout: ({ className, style, ...props }: CalloutProps) => (
+      <BaseCallout
+        {...props}
+        className={['break-words', className].filter(Boolean).join(' ')}
+        style={{ wordBreak: 'break-word', overflowWrap: 'anywhere', ...style }}
+      />
+    ),
     img: (props) => <ImageZoom {...(props as any)} />,
     ...components,
   };
