cc428a3555da054dbb4ba00307f3d912726f769e
lin <kingxl777@gmail.com>
2026-01-05 23:39:51 +0800

    update the path of head img

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -5,22 +5,7 @@ import type { ComponentProps } from 'react';
 import type { MDXComponents } from 'mdx/types';
 import { Mermaid } from '@/components/mdx/mermaid';
 import * as TabsComponents from 'fumadocs-ui/components/tabs';
-import { Image as FrameworkImage } from 'fumadocs-core/framework';
-import iconQuestion from '@/public/images/icon-question.png';
-import iconLinks from '@/public/images/icon-links.png';
-import iconOffenseFaas from '@/public/images/icon-offense-faas.png';
-import iconDefenseShield from '@/public/images/icon-defense-shield.png';
-import iconBprotocol from '@/public/images/icon-bprotocol.png';
-import introIcon from '@/public/images/intro.png';
-
-const headingIconMap: Record<string, any> = {
-  '/images/icon-question.png': iconQuestion,
-  '/images/icon-links.png': iconLinks,
-  '/images/icon-offense-faas.png': iconOffenseFaas,
-  '/images/icon-defense-shield.png': iconDefenseShield,
-  '/images/icon-bprotocol.png': iconBprotocol,
-  '/images/intro.png': introIcon,
-};
+import { withBasePath } from '@/lib/env';
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
@@ -35,24 +20,21 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
       />
     ),
     img: (props) => {
-      const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '/mev-wiki';
       const { src, className, alt, ...restProps } = props as any;
 
       // For heading icons, add basePath to the src
       if (className?.includes('heading-icon') && src && typeof src === 'string') {
-        const headingIconSize = 32;
-        const normalizedSrc = src.startsWith('/') ? src : `/${src}`;
-        const resolvedSrc = headingIconMap[normalizedSrc] ?? `${basePath}${normalizedSrc}`;
+        const resolvedSrc = withBasePath(src);
 
         return (
-          <FrameworkImage
+          // eslint-disable-next-line @next/next/no-img-element
+          <img
             {...restProps}
             src={resolvedSrc}
             alt={alt ?? ''}
             className={className}
-            width={headingIconSize}
-            height={headingIconSize}
-            sizes={`${headingIconSize}px`}
+            width={32}
+            height={32}
           />
         );
       }
